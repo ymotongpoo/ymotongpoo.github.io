495d4ce8aa94906c6488f9a5e5cf5defa7e056bb
Yoshifumi YAMAGUCHI <ymotongpoo@gmail.com>
2016-12-27 12:49:15 +0900

    [source] 20161227124914 : bat

--- build.sh
+++ build.sh
@@ -2,28 +2,28 @@
 
 theme=tropic
 date=`date +"%Y%m%d%H%M%S"`
-tmp="/tmp/$date"
+tmp="/tmp/${date}"
 
 function _deploy() {
   echo -e "Build and deploy to GitHub"
 
   # build
   rm -rf public/*
-  hugo --theme="$theme"
+  hugo --theme="${theme}"
 
   # git add, commit and push to source
   git add --all
-  git commit -m "[source] $date : $*"
+  git commit -m "[source] ${date} : $*"
   git push origin source:source
 
   # git add, commit and push to master
-  mkdir -p $tmp
-  cp -r public/. $tmp
+  mkdir -p ${tmp}
+  cp -r public/. ${tmp}
   rm -rf public
   git checkout master
-  cp -rf $tmp/. .
+  cp -rf ${tmp}/. .
   git add --all
-  git commit -m "[site] $date : $*"
+  git commit -m "[site] ${date} : $*"
   git push origin master:master
   git checkout source
 
@@ -32,6 +32,19 @@ function _deploy() {
 
 function _serve() {
   env HUGO_ENV="DEV" hugo server --theme="$theme" --watch --buildDrafts=true --buildFuture=true
+
+  return 0
+}
+
+function _new() {
+  local category=$1
+  shift
+  local prefix=`date +"%Y-%m-%d"`
+  slug=""
+  for w in $*; do
+    slug=${slug}"-"${w}
+  done
+  hugo new "${category}/${prefix}${slug}"
 
   return 0
 }
@@ -47,6 +60,14 @@ case "$1" in
     shift
     _deploy $*
     ;;
+  "diary")
+    shift
+    _new "dairy" $*
+    ;;
+  "tech")
+    shift
+    _new "tech" $*
+    ;;
   "serve")
     shift
     _serve
@@ -56,4 +77,3 @@ case "$1" in
     _usage
     ;;
 esac
-
